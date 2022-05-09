77f38b6adb1920cb44bfa02bb19e1089dfcac5bd
Kushal Sharma <kdgrtsharma99@gmail.com>
2022-05-09 12:42:16 +0530

    Update flappy_bird.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -334,9 +334,11 @@ def eval_genomes(genomes, config):
         draw_window(WIN, birds, pipes, base, score, gen, pipe_ind)
 
         # break if score gets large enough
-        '''if score > 20:
+        breakpoint=20
+        if score > breakpoint:
+            breakpoint+=5
             pickle.dump(nets[0],open("best.pickle", "wb"))
-            break'''
+            break
 
 
 def run(config_file):
